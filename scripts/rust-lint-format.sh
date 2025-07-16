10993b47e2c5ac4a81d7f32f2809ee242b300ea9
Samer <samerweb3@gmail.com>
2025-07-16 22:53:27 +0200

    feat: Add build commands for canister dashboard and release build

--- scripts/rust-lint-format.sh
+++ scripts/rust-lint-format.sh
@@ -5,3 +5,6 @@ cargo fmt
 cargo clippy --all-targets --all-features -- -D warnings
 cargo check --all-targets --all-features
 cargo test --workspace --exclude my-wasm-test
+
+npm run build --workspace=canister-dashboard-frontend
+cargo build --release -p my-canister-dashboard
