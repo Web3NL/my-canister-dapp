99914aa77f69e49cbf59b789d7619fa317d396d2
Samer <samerweb3@gmail.com>
2025-07-21 16:30:55 +0200

    fix: update FAQ section to enhance clarity and provide detailed explanations about Canister Dapps

--- my-canister-app/src/routes/faq/+page.svelte
+++ my-canister-app/src/routes/faq/+page.svelte
@@ -1,9 +1,57 @@
 <script lang="ts">
-  import { Collapsible } from '@dfinity/gix-components';
+  import { Collapsible, Section } from '@dfinity/gix-components';
 </script>
 
 <div class="wrapper">
   <h1>FAQ</h1>
+
+  <Section>
+    <h2 slot="title">Why do we need Canister Dapps?</h2>
+    <p>
+      The <a
+        href="https://internetcomputer.org"
+        target="_blank"
+        rel="noopener noreferrer">Internet Computer</a
+      >
+      is a versatile blockchain that allows for varying degrees of security, user
+      privacy and ownership.
+    </p>
+    <p>
+      For starters, it's a publicly governed blockchain with a 'liquid
+      democracy'. Second, the security and authentication model of the IC
+      together with
+      <a
+        href="https://identity.internetcomputer.org"
+        target="_blank"
+        rel="noopener noreferrer">Internet Identity</a
+      >
+      are ahead of legacy Web2 technology by default, giving users more control over
+      their data and on-chain assets. For example, the
+      <a
+        href="https://internetcomputer.org/docs/references/icrc1-standard"
+        target="_blank"
+        rel="noopener noreferrer">ICRC-1</a
+      >
+      standard uses the IC's caller authentication model when sending funds. Furthermore,
+      the ecosystem allows for
+      <a
+        href="https://wiki.internetcomputer.org/wiki/Service_Nervous_System"
+        target="_blank"
+        rel="noopener noreferrer">SNS DAOs</a
+      > to give users more guarantees and ownership stake in certain projects.
+    </p>
+    <p>
+      Despite these advantages, in practice it is still nearly impossible for
+      non-technical users to be in full control of a single canister that runs
+      their app.
+    </p>
+    <p>
+      Canister Dapps allow non-technical users to create and fully own their
+      decentralized applications. This allows for true decentralized software
+      without any intermediaries.
+    </p>
+  </Section>
+
   <div class="faq-item">
     <Collapsible iconSize="medium">
       <h3 slot="header">What is a Canister Dapp?</h3>
@@ -47,55 +95,6 @@
         > to create a new canister with their own funds, install a Canister Dapp
         and be the sole controller. The user can use the Canister Dashboard to top-up
         their Dapp with cycles and perform other management tasks.
-      </p>
-    </Collapsible>
-  </div>
-
-  <div class="faq-item">
-    <Collapsible iconSize="medium">
-      <h3 slot="header">Why do we need Canister Dapps?</h3>
-      <p>
-        The <a
-          href="https://internetcomputer.org"
-          target="_blank"
-          rel="noopener noreferrer">Internet Computer</a
-        >
-        is a versatile blockchain that allows for varying degrees of security, user
-        privacy and ownership.
-      </p>
-      <p>
-        For starters, it's a publicly governed blockchain with a 'liquid
-        democracy'. Second, the security and authentication model of the IC
-        together with
-        <a
-          href="https://identity.internetcomputer.org"
-          target="_blank"
-          rel="noopener noreferrer">Internet Identity</a
-        >
-        are ahead of legacy Web2 technology by default, giving users more control
-        over their data and on-chain assets. For example, the
-        <a
-          href="https://internetcomputer.org/docs/references/icrc1-standard"
-          target="_blank"
-          rel="noopener noreferrer">ICRC-1</a
-        >
-        standard uses the IC's caller authentication model when sending funds. Furthermore,
-        the ecosystem allows for
-        <a
-          href="https://wiki.internetcomputer.org/wiki/Service_Nervous_System"
-          target="_blank"
-          rel="noopener noreferrer">SNS DAOs</a
-        > to give users more guarantees and ownership stake in certain projects.
-      </p>
-      <p>
-        Despite these advantages, in practice it is still nearly impossible for
-        non-technical users to be in full control of a single canister that runs
-        their app.
-      </p>
-      <p>
-        Canister Dapps allow non-technical users to create and fully own their
-        decentralized applications. This allows for true decentralized software
-        without any intermediaries.
       </p>
     </Collapsible>
   </div>
@@ -156,10 +155,11 @@
     <Collapsible iconSize="medium">
       <h3 slot="header">Does mycanister.app store user data?</h3>
       <p>
-        No, mycanister.app does not store any user data. The platform is
-        designed to give users full control over their decentralized
-        applications without relying on an intermediary. The user will be able
-        to run mycanister.app from their local computer if they wish.
+        No, mycanister.app does not store any user data. It's simply a static
+        website. The platform is designed to give users full control over their
+        decentralized applications without relying on an intermediary. The user
+        will be able to run mycanister.app from their local computer if they
+        wish.
       </p>
     </Collapsible>
   </div>
@@ -184,6 +184,7 @@
   }
 
   .faq-item {
-    margin-bottom: 3rem;
+    margin-bottom: 2rem;
+    margin-top: 2rem;
   }
 </style>
