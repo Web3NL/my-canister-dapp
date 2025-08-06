e509f4f393943ca61dabf88805f2249efc67252d
Samer <samerweb3@gmail.com>
2025-08-06 17:08:01 +0200

    fix: update path for canister dashboard frontend in validation script

--- validate-and-test-all.sh
+++ validate-and-test-all.sh
@@ -20,7 +20,7 @@ if [ "$CLEAN_FLAG" = "true" ]; then
     ./scripts/clean.sh
 fi
 
-./scripts/create-canister-dapp-dev-env.sh my-canister-dapp-js/canister-dashboard-frontend
+./scripts/create-canister-dapp-dev-env.sh my-canister-dapp-js/canister-dashboard-frontend/src
 ./scripts/create-canister-dapp-dev-env.sh examples/my-hello-world/src/my-hello-world-frontend
 
 ./scripts/check.sh
