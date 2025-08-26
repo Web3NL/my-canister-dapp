6f8ea52adc253544fc30e81d77578f11a86c801c
Samer <samerweb3@gmail.com>
2025-08-26 11:31:20 +0200

    add ledger and internet identoty to dfx-env

---
+++ dfx-env/README.md
@@ -0,0 +1,19 @@
+## ICP Ledger
+
+### Candid
+
+https://raw.githubusercontent.com/dfinity/ic/5ba1412f9175d987661ae3c0d8dbd1ac3e092b7d/rs/rosetta-api/icp_ledger/ledger.did
+
+### WASM
+
+https://download.dfinity.systems/ic/5ba1412f9175d987661ae3c0d8dbd1ac3e092b7d/canisters/ledger-canister.wasm.gz
+
+## Internet Identity
+
+### Candid
+
+https://raw.githubusercontent.com/dfinity/internet-identity/release-2023-05-30/src/internet_identity/internet_identity.did
+
+### WASM
+
+https://github.com/dfinity/internet-identity/releases/download/release-2023-05-30/internet_identity_production.wasm.gz
---
+++ dfx-env/deploy-local-ledger.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if ! dfx identity list | grep -q minter; then
+    # If minter is not found, run the command
+    dfx identity new minter
+fi
+
+export MINTER_ACCOUNT_ID=$(dfx ledger account-id --identity minter)
+export DEFAULT_ACCOUNT_ID=$(dfx ledger account-id --identity ident-1)
+
+dfx deploy icp-ledger --specified-id ryjl3-tyaaa-aaaaa-aaaba-cai --argument "
+  (variant {
+    Init = record {
+      minting_account = \"$MINTER_ACCOUNT_ID\";
+      initial_values = vec {
+        record {
+          \"$DEFAULT_ACCOUNT_ID\";
+          record {
+            e8s = 10_000_000_000 : nat64;
+          };
+        };
+      };
+      send_whitelist = vec {};
+      transfer_fee = opt record {
+        e8s = 10_000 : nat64;
+      };
+      token_symbol = opt \"LICP\";
+      token_name = opt \"Local ICP\";
+    }
+  })
+"
