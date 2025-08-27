3897f12f6f1b86ba9a2b716030499e173e00001e
Samer <samerweb3@gmail.com>
2025-08-27 20:10:26 +0200

    chore: update scripts and env setup

--- scripts/rust-lint-format.sh
+++ scripts/rust-lint-format.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
 
+# Main workspace
 cargo fmt
 cargo clippy --all-targets --all-features -- -D warnings
 cargo check --all-targets --all-features
 cargo test --workspace --exclude canister-dapp-test
+
+# dfx-env workspace
+cd dfx-env
+cargo fmt
+cargo clippy --all-targets --all-features -- -D warnings
+cargo check --all-targets --all-features
+cargo test --workspace
+cd ..
