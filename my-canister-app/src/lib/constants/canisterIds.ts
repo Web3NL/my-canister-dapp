7dfccb0fcb2ebe6691f363fa14a3d4583aaa33fc
Samer <samerweb3@gmail.com>
2025-08-29 14:16:16 +0200

    fix: revert unwanted changes

--- my-canister-app/src/lib/constants/canisterIds.ts
+++ my-canister-app/src/lib/constants/canisterIds.ts
@@ -1,8 +1,3 @@
-const INDEX_CANISTER_ID_LOCAL = 'qhbym-qaaaa-aaaaa-aaafq-cai';
-const INDEX_CANISTER_ID_MAINNET = 'qhbym-qaaaa-aaaaa-aaafq-cai';
-
-export const INDEX_CANISTER_ID = PROD
-  ? INDEX_CANISTER_ID_MAINNET
-  : INDEX_CANISTER_ID_LOCAL;
+export const INDEX_CANISTER_ID = 'qhbym-qaaaa-aaaaa-aaafq-cai';
 
 export const CMC_CANISTER_ID = 'rkp4c-7iaaa-aaaaa-aaaca-cai';
