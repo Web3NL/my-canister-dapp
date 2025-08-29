0e53a024e2dcf0111ce19f86411e8b4e13729e7e
Samer <samerweb3@gmail.com>
2025-08-29 16:53:26 +0200

    fix: remove reliance on ic-wasm

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -6,5 +6,7 @@ echo "Unit testing my-canister-dashboard"
 npm run test --workspace=@web3nl/my-canister-dashboard
 
 echo "🧪 Running Canister Dapp Test"
-./scripts/build-examples.sh
+DAPP_BUILD_MODE=prod dfx build my-hello-world
+DAPP_BUILD_MODE=prod ./scripts/copy-example-wasm.sh
+
 cargo test -p canister-dapp-test
