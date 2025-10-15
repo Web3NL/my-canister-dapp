d087ab2975a370a02dd6c1475dc9919d014a7400
Samer <samerweb3@gmail.com>
2025-10-15 21:34:10 +0200

    chore: turn on mmode

--- my-canister-app/src/lib/stores/maintenance.ts
+++ my-canister-app/src/lib/stores/maintenance.ts
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store';
 
 // Toggle maintenance mode for the application
-export const maintenanceMode = writable(false);
+export const maintenanceMode = writable(true);
