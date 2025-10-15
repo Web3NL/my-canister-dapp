60c43a99053effca06b0a40c76f44291dbacf51a
Samer <samerweb3@gmail.com>
2025-10-15 22:45:35 +0200

    Merge pull request #192 from Web3NL:chore--update-deps
    Chore--update-deps

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -5,5 +5,8 @@ set -e
 echo "Unit testing my-canister-dashboard"
 npm run test --workspace=@web3nl/my-canister-dashboard
 
+echo "Unit testing canister-dapp-installer"
+npm run test --workspace=canister-dapp-installer
+
 echo "Wasm testing with canister-dapp-test"
 cargo test -p canister-dapp-test
